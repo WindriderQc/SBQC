83aa3ff54d8860fe30d87566c86d7c74cf9adf8f
yb <yanikbeaulieu@hotmail.com>
2021-02-26 13:55:00 -0500

    fileAccess

--- public/earthExplorer.js
+++ public/earthExplorer.js
@@ -90,3 +90,22 @@ async function getISS()
 
 getISS()
 setInterval(getISS, 1000)
+
+
+
+
+function ipLookUp () {
+    $.ajax('http://ip-api.com/json')
+    .then(
+        function success(response) {
+            console.log('User\'s Location Data is ', response);
+            console.log('User\'s Country', response.country);
+        },
+  
+        function fail(data, status) {
+            console.log('Request failed.  Returned status of',
+                        status);
+        }
+    );
+  }
+  ipLookUp()
