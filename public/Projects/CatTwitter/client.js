0c33d11aa592fe3ecf1888275009d5b802b231f3
yb <yanikbeaulieu@hotmail.com>
2021-10-21 16:08:22 -0400

    cat client URL to localhost

--- public/Projects/CatTwitter/client.js
+++ public/Projects/CatTwitter/client.js
@@ -5,8 +5,9 @@ const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
 const mewsElement = document.querySelector('.mews');
 const loadMoreElement = document.querySelector('#loadMore');
-const API_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3001/meows/v2/mews' : 'https://meower-api.now.sh/v2/mews';
-//const API_URL = 'http://192.168.0.33:3001/meows/v2/mews' 
+
+//const API_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3001/meows/v2/mews' : 'https://meower-api.now.sh/v2/mews';
+const API_URL = 'http://localhost:3001/meows/v2/mews' 
 
 let skip = 0;
 let limit = 20;  //   TODO:  si le limit est plus petit que window et y apas de scroll....   le scroll event launch pas
