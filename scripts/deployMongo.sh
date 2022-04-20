14b6f994670b0c53af9703f1f4d40e2603c1f9c4
yb <yanikbeaulieu@hotmail.com>
2022-04-20 13:48:28 -0400

    deployMongo

--- scripts/deployMongo.sh
+++ scripts/deployMongo.sh
@@ -6,3 +6,49 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
+
+systemctl enable mongod.service    #  insure launch at startup
+
+
+
+#Mongo authentication
+
+in /etc/mongod.conf    change bindip to 0.0.0.0
+uncomment security and use:
+security: 
+authorization: enabled
+
+on terminal  type  mongo
+
+use admin
+db.createUser(
+    {
+        user: "superuser",
+        pwd: "changeMeAsStrongPassword",
+        roles:  [ "root" ]
+    }
+  )
+
+  restart db. *** bug*** use  admin; db.auth(USER_NAME, PASS_WORD)
+
+
+
+
+
+
+*** bug ***
+  Just do those two commands for temporary solution:
+
+sudo rm -rf /tmp/mongodb-27017.sock
+sudo service mongod start
+For details:
+
+That shall be fault due to user permissions in .sock file, You may have to change the owner to monogdb user.
+
+sudo chown -R mongodb:mongodb /var/lib/mongodb
+sudo chown mongodb:mongodb /tmp/mongodb-27017.sock
+
+
+
+
+hostnamectl      donne info cool et bref
