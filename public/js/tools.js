c282a3684dae0ed134d722c50c030b3f928ec2e3
Yanik Beaulieu <yanikbeaulieu@hotmail.com>
2025-01-28 17:47:25 +0000

    centralizing tools.js    and installWebAdmin script

--- public/js/tools.js
+++ public/js/tools.js
@@ -265,6 +265,21 @@ const Tools = {
                 return false;
         },
 
+
+        
+        recursiveCircle: (x,y,d, minD = 10) =>  { //  min diameter is 10 pixels per default
+            stroke(125);
+            noFill();
+
+            ellipse(x,y, d, d);
+
+            if(d > minD) {
+                recursiveCircle(x + d/2,y,d/2);
+                recursiveCircle(x - d/2,y,d/2);
+            }
+
+        },  
+
         
     }
 
@@ -272,3 +287,91 @@ const Tools = {
 
 // find if a point (x,y) is within a circle:
 //si  RacineCarré de (x^2 + y^2)    <= Rayon
+
+
+
+
+
+
+
+
+
+/*
+String url = "http://www.imdb.com/title/tt0058331";
+String[] lines = loadStrings(url);
+// Get rid of the array in order to search the whole page
+String html = join(lines, " ");
+
+// Searching for running time
+String start = ""; 
+
+String end = "";
+String runningtime = giveMeTextBetween(html, start, end);
+println(runningtime);
+
+
+
+
+// *The following code retrieves both the running time and movie poster iamge from IMDb and displays it onscreen.
+
+String runningtime;
+PImage poster;
+
+void setup() {
+  size(300, 350);
+  loadData();
+}
+
+void draw() {
+  // Display all the stuff I want to display
+  background(255);
+  image(poster, 10, 10, 164, 250);
+  fill(0);
+  text("Shaun the Sheep", 10, 300);
+  text(runningtime, 10, 320);
+}
+
+void loadData() {
+  String url = "http://www.imdb.com/title/tt2872750/";
+
+  // Get the raw HTML source into an array of strings (each line is one element in the array).
+  // The next step is to turn array into one long string with join().
+  String[] lines = loadStrings(url);
+  String html = join(lines, "");
+
+  String start = "";
+  String end = "";
+  runningtime = giveMeTextBetween(html, start, end);Searching for running time.
+
+  start = "";
+  // Search for the URL of the poster image.
+  String imgUrl = giveMeTextBetween(html, start, end);
+  // Now, load that image!
+  poster = loadImage(imgUrl);
+}
+
+String giveMeTextBetween(String s, String before, String after) {
+
+  // This function returns a substring between two substrings (before and after).
+  //  If it can’t find anything it returns an empty string.
+  String found = "";
+
+  // Find the index of before
+  int start = s.indexOf(before);     
+  if (start == -1) {
+    return "";                       
+  }    
+
+  // Move to the end of the beginning tag
+  // and find the index of the "after" String      
+  start += before.length();    
+  int end = s.indexOf(after, start); 
+  if (end == -1) {
+    return "";                       
+  }
+
+  // Return the text in between
+  return s.substring(start, end); 
+}
+*/
+
