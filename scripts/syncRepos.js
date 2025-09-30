e8b89971d225b3b38e0741b8e14bb637a9a2f2c0
yanik beaulieu <yanikbeaulieu@hotmail.com>
2025-09-30 03:20:25 +0000

    files clean up

---
+++ scripts/syncRepos.js
@@ -0,0 +1,43 @@
+// scripts/syncRepos.js
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+// List your repos here
+const repos = [
+  "https://github.com/WindriderQc/mqttTest.git",
+  "https://github.com/WindriderQc/DrawTogether.git",
+  "https://github.com/WindriderQc/PhotoBox.git",
+  "https://github.com/WindriderQc/Tides.git",
+];
+
+const baseDir = path.join(__dirname, "..", "public", "projects");
+
+if (!fs.existsSync(baseDir)) {
+  fs.mkdirSync(baseDir, { recursive: true });
+}
+
+function syncRepos() {
+  repos.forEach(repo => {
+    const repoName = path.basename(repo, ".git");
+    const targetDir = path.join(baseDir, repoName);
+
+    if (fs.existsSync(targetDir)) {
+      console.log(`Updating ${repoName}...`);
+      try {
+        execSync("git pull", { cwd: targetDir, stdio: "inherit" });
+      } catch (err) {
+        console.error(`Failed to pull ${repoName}:`, err.message);
+      }
+    } else {
+      console.log(`Cloning ${repoName}...`);
+      try {
+        execSync(`git clone ${repo} ${targetDir}`, { stdio: "inherit" });
+      } catch (err) {
+        console.error(`Failed to clone ${repoName}:`, err.message);
+      }
+    }
+  });
+}
+
+module.exports = syncRepos;
