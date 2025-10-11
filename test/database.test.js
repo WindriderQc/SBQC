beaefa9aefb84f8a527f479fe3da2269e2624bc1
yanik beaulieu <yanikbeaulieu@hotmail.com>
2025-10-11 06:26:40 +0000

    test reset...

--- test/database.test.js
+++ test/database.test.js
@@ -71,19 +71,16 @@ describe('Database Module', function() {
     });
 
     describe('loadCollections', () => {
-        it('should load collections and boot data into app.locals', async () => {
-            await db.createCollection('test_collection');
-
+        it('should initialize app.locals.collections for DataAPI architecture', async () => {
+            // Since SBQC now uses DataAPI, loadCollections just initializes an empty collections object
             await loadCollections(db, app);
 
-            expect(app.locals.collections).to.have.property('test_collection');
-            expect(app.locals.collections).to.have.property('boot');
-            expect(app.locals.collectionInfo).to.have.property('test_collection');
-            expect(app.locals.collectionInfo.test_collection).to.equal(0);
-
-            const bootRecord = await app.locals.collections.boot.findOne({ client: 'server' });
-            expect(bootRecord).to.not.be.null;
-            expect(bootRecord.content).to.equal('dbServer boot');
+            // The collections object should exist but be empty (DataAPI handles data operations)
+            expect(app.locals.collections).to.be.an('object');
+            expect(Object.keys(app.locals.collections).length).to.equal(0);
+            
+            // No boot record or collection info is created with DataAPI architecture
+            // This is expected behavior - local MongoDB is only for sessions
         });
     });
 });
